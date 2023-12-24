1c4d80a41f1091ea7b0ba8141ae7534bb4579411
vhing004 <hvinh12112004@gmail.com>
2023-12-24 12:16:48 +0700

    update continues

--- js/app.js
+++ js/app.js
@@ -8,6 +8,9 @@ $(document).ready(function () {
         "<button type='button' class='slick-prev pull-left arrow'><i class='fa-solid fa-angle-left'></i></button>",
       nextArrow:
         "<button type='button' class='slick-next pull-right arrow'><i class='fa-solid fa-angle-right'></i></button>",
+      prevArrow: false,
+      nextArrow: false,
+      draggable: true,
       responsive: [
         {
           breakpoint: 500,
@@ -38,19 +41,4 @@ $(document).ready(function () {
     });
   });
 
-  $(document).ready(function () {
-    // handle header nav
-    // $('.js-dropdown-toggle').on('click', function () {
-    //   $(this).toggleClass('active').siblings('.js-dropdown-menu').slideToggle();
-    // });
-    // open and close mobile menu
-    $('.js-menu-btn').on('click', function () {
-      $('.js-mobile-menu, .js-mobile-filter').addClass('is-show');
-      $('body').addClass('no-scroll');
-    });
-    $('.js-mobile-filter, .js-menu-close').on('click', function () {
-      $('.js-mobile-menu, .js-mobile-filter').removeClass('is-show');
-      $('body').removeClass('no-scroll');
-    });
-  });
   
